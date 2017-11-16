9b4d6ea2d1d37af16e2f7008077b48b93826995c
Alex Siryi <desertkun@gmail.com>
2017-11-16 11:40:17 +0200

    Updated await

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ Test.allow_session = true;
 Test.prototype.main = async function(args)
 {
     // fetch the events
-    var events_list = event.list();
+    var events_list = await event.list();
     // feed them back to the user
     return events_list;
 };
